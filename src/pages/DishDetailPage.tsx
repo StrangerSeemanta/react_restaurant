739f08ba540c43d50e83611cd8f981b27bf6f95d
Seemanta Sarker <91196514+StrangerSeemanta@users.noreply.github.com>
2023-11-28 03:19:31 +0600

    productPage(dynamic) loader added and when the image will load it will stop visual loading

--- src/pages/DishDetailPage.tsx
+++ src/pages/DishDetailPage.tsx
@@ -21,6 +21,8 @@ function Layout({ product }: layoutProps) {
     const [mainColor, setMainColor] = useState<string>("#fff");
     const [secondColor, setSecondColor] = useState('')
     const [thirdColor, setThirdColor] = useState('#000')
+    const [isLoading, setLoading] = useState(true);
+
     useEffect(() => {
         if (image.current) {
             ExtractColor(image.current)
@@ -29,6 +31,11 @@ function Layout({ product }: layoutProps) {
                     setSecondColor(response[2].hex)
                     setThirdColor(response[1].hex)
                 })
+            const img = new Image();
+            img.src = image.current?.src;
+            img.onload = () => {
+                setLoading(false)
+            }
         }
 
         const handleScroll = () => {
@@ -69,82 +76,87 @@ function Layout({ product }: layoutProps) {
 
     return (
         <>
-            <Stack width={"100%"} minHeight={"90vh"} px={3} direction={{ xs: "column", md: "row" }} alignItems={"center"} py={{ xs: 4, md: 0 }} justifyContent={{ xs: "center", md: "space-between" }} sx={{ background: `${secondColor}24` }}>
-                <Box width={{ xs: "100%", md: "40%" }}>
-                    <Typography variant='h1' fontSize={{ xs: "3rem", md: "7rem" }} width={"100%"} textAlign={"left"} color={mainColor} fontWeight={600}>
-                        {product.title}
+            {isLoading &&
+                <Fragment>
+                    <Loader />
+                </Fragment>
+            }
+            <Fragment>
+                <Stack width={"100%"} minHeight={"90vh"} px={3} direction={{ xs: "column", md: "row" }} alignItems={"center"} py={{ xs: 4, md: 0 }} justifyContent={{ xs: "center", md: "space-between" }} sx={{ background: `${secondColor}24` }}>
+                    <Box width={{ xs: "100%", md: "40%" }}>
+                        <Typography variant='h1' fontSize={{ xs: "3rem", md: "7rem" }} width={"100%"} textAlign={"left"} color={mainColor} fontWeight={600}>
+                            {product.title}
 
-                    </Typography>
-                    <Typography variant='h6' fontWeight={600} color={secondColor}>
-                        your tasty {product.title.toLowerCase()} is waiting ...
-                    </Typography>
-                </Box>
-                <Box height={{ xs: "70%", md: "75vh" }} width={{ xs: "70%", md: "50%" }} sx={{ transition: 'all 0.3s ease', transform: isFixed ? `scale(0.6) translateY(${translateY}px) translateX(${translateX}px)` : 'none', filter: isFixed ? "grayscale(1)" : "grayscale(0)", }} position={isFixed ? "fixed" : "relative"}>
-                    <img ref={image} src={product.img} alt={`image of ${product.title}`} style={{ width: "100%", height: "100%", objectFit: "contain", filter: "drop-shadow(20px 45px 13px  rgba(0,0,0,0.26)" }} />
-                </Box>
-            </Stack>
-
-            <Stack ref={detailDiv} position={"relative"} component={"div"} zIndex={9999} width={"100%"} minHeight={"90vh"} px={3} direction={"column"} justifyContent={"center"} alignItems={"center"} sx={{ background: `${thirdColor}54` }}>
-                <Typography variant='h2' fontSize={{ xs: "2.5rem", md: "4rem" }} width={"100%"} textAlign={"center"} color={mainColor} fontWeight={600}>
-                    Taste the
-
-                </Typography>
-                <Typography className='product_country_name'
-                    fontFamily={"poppins"} variant='h1'
-                    fontSize={{ xs: "3rem", md: "7rem" }}
-                    width={"100%"}
-                    textAlign={"center"}
-                    color={secondColor}
-                    fontWeight={600}
-                >
-                    {product.country}
-
-                </Typography>
-                <Typography variant='h2' fontSize={{ xs: "2.5rem", md: "4rem" }} width={"100%"} textAlign={"center"} color={mainColor} fontWeight={600}>
-                    {product.catagory}
-
-                </Typography>
-
-            </Stack>
-            <Stack component={"div"} ref={priceDiv} className='productpage_price_section' color={"red"} data-price={`$${product.price}`} spacing={2} width={"100%"} minHeight={"90vh"} px={3} justifyContent={"center"} alignItems={"center"} sx={{ background: `${secondColor}64` }}>
-                <Typography variant='h2' fontFamily={'lilita one'} fontSize={{ xs: "4rem", md: "8rem" }} width={"100%"} textAlign={"center"} color={mainColor} >
-                    Only For
-
-                </Typography>
-                <Typography variant='h2' fontFamily={'lilita one'} fontSize={{ xs: "4rem", md: "8rem" }} width={"100%"} textAlign={"center"} color={"dodgerblue"} sx={{ borderBottom: `thick solid ${mainColor}` }} >
-                    {`$${product.price}`}
-
-                </Typography>
-
-
-            </Stack>
-            <Stack component={"div"} position={"relative"} color={"red"} spacing={2} width={"100%"} minHeight={"100vh"} px={3} justifyContent={"center"} alignItems={"center"} sx={{ background: `${mainColor}34` }}>
-                {product.special && <Box width={"85%"} height={"10vh"} >
-                    <Marquee config={{ speed: 10000 }} customStyle={{ textAlign: "center", fontSize: 30, fontWeight: 600, textTransform: "uppercase", color: "red" }}>
-                        It is one of our <span style={{ color: "dodgerblue" }}>special</span>  and <span style={{ color: "orange" }}>Hot</span>  item. You need to taste it soon.
-                    </Marquee>
-                </Box>}
-                <Stack alignItems={"center"} spacing={2} bgcolor={"white"} px={7} py={10} sx={{ boxShadow: "12px 12px 15px rgba(0,0,0,0.15)", borderRadius: 2 }}>
-                    <Rating size='large' value={product.ratings} readOnly />
-                    <Typography fontSize={24} fontFamily={'"poppins"'} color={"dodgerblue"} textAlign={"center"}>
-                        Reviewed By <strong style={{ fontSize: 30 }}>{product.reviews}</strong>  customers
-                    </Typography>
+                        </Typography>
+                        <Typography variant='h6' fontWeight={600} color={secondColor}>
+                            your tasty {product.title.toLowerCase()} is waiting ...
+                        </Typography>
+                    </Box>
+                    <Box height={{ xs: "70%", md: "75vh" }} width={{ xs: "70%", md: "50%" }} sx={{ transition: 'all 0.3s ease', transform: isFixed ? `scale(0.6) translateY(${translateY}px) translateX(${translateX}px)` : 'none', filter: isFixed ? "grayscale(1)" : "grayscale(0)", }} position={isFixed ? "fixed" : "relative"}>
+                        <img ref={image} src={product.img} alt={`image of ${product.title}`} style={{ width: "100%", height: "100%", objectFit: "contain", filter: "drop-shadow(20px 45px 13px  rgba(0,0,0,0.26)" }} />
+                    </Box>
                 </Stack>
 
-            </Stack >
+                <Stack ref={detailDiv} position={"relative"} component={"div"} zIndex={9999} width={"100%"} minHeight={"90vh"} px={3} direction={"column"} justifyContent={"center"} alignItems={"center"} sx={{ background: `${thirdColor}54` }}>
+                    <Typography variant='h2' fontSize={{ xs: "2.5rem", md: "4rem" }} width={"100%"} textAlign={"center"} color={mainColor} fontWeight={600}>
+                        Taste the
+
+                    </Typography>
+                    <Typography className='product_country_name'
+                        fontFamily={"poppins"} variant='h1'
+                        fontSize={{ xs: "3rem", md: "7rem" }}
+                        width={"100%"}
+                        textAlign={"center"}
+                        color={secondColor}
+                        fontWeight={600}
+                    >
+                        {product.country}
+
+                    </Typography>
+                    <Typography variant='h2' fontSize={{ xs: "2.5rem", md: "4rem" }} width={"100%"} textAlign={"center"} color={mainColor} fontWeight={600}>
+                        {product.catagory}
+
+                    </Typography>
+
+                </Stack>
+                <Stack component={"div"} ref={priceDiv} className='productpage_price_section' color={"red"} data-price={`$${product.price}`} spacing={2} width={"100%"} minHeight={"90vh"} px={3} justifyContent={"center"} alignItems={"center"} sx={{ background: `${secondColor}64` }}>
+                    <Typography variant='h2' fontFamily={'lilita one'} fontSize={{ xs: "4rem", md: "8rem" }} width={"100%"} textAlign={"center"} color={mainColor} >
+                        Only For
+
+                    </Typography>
+                    <Typography variant='h2' fontFamily={'lilita one'} fontSize={{ xs: "4rem", md: "8rem" }} width={"100%"} textAlign={"center"} color={"dodgerblue"} sx={{ borderBottom: `thick solid ${mainColor}` }} >
+                        {`$${product.price}`}
+
+                    </Typography>
+
+
+                </Stack>
+                <Stack component={"div"} position={"relative"} color={"red"} spacing={2} width={"100%"} minHeight={"100vh"} px={3} justifyContent={"center"} alignItems={"center"} sx={{ background: `${mainColor}34` }}>
+                    {product.special && <Box width={"85%"} height={"10vh"} >
+                        <Marquee config={{ speed: 10000 }} customStyle={{ textAlign: "center", fontSize: 30, fontWeight: 600, textTransform: "uppercase", color: "red" }}>
+                            It is one of our <span style={{ color: "dodgerblue" }}>special</span>  and <span style={{ color: "orange" }}>Hot</span>  item. You need to taste it soon.
+                        </Marquee>
+                    </Box>}
+                    <Stack alignItems={"center"} spacing={2} bgcolor={"white"} px={7} py={10} sx={{ boxShadow: "12px 12px 15px rgba(0,0,0,0.15)", borderRadius: 2 }}>
+                        <Rating size='large' value={product.ratings} readOnly />
+                        <Typography fontSize={24} fontFamily={'"poppins"'} color={"dodgerblue"} textAlign={"center"}>
+                            Reviewed By <strong style={{ fontSize: 30 }}>{product.reviews}</strong>  customers
+                        </Typography>
+                    </Stack>
+
+                </Stack >
+
+            </Fragment>
         </>
     )
 }
 function DishDetailPage() {
     const { dishURL } = useParams();
-    const [loading, setLoading] = useState(true);
     const [productDetails, setProductDetails] = useState<DataProps | null>(null);
 
     useEffect(() => {
         if (dishURL) {
-            const loadingTimeoutId = setTimeout(() => {
-                setLoading(false);
-            }, 1500); // 1000 milliseconds (1 second)
+
             const matchingProduct = Data.find(
                 (product) => product.title.toLowerCase() === decodeURIComponent(dishURL).toLowerCase()
             );
@@ -153,7 +165,6 @@ function DishDetailPage() {
                 setProductDetails(matchingProduct);
             }
 
-            return () => clearTimeout(loadingTimeoutId);
 
         }
 
@@ -162,14 +173,12 @@ function DishDetailPage() {
 
     return (
         <Fragment>
-            {loading ? (
-                <Loader />
-            ) :
-                (productDetails ? (
-                    <Layout product={productDetails} />
-                ) : (
-                    <Nopage />
-                ))}
+
+            {(productDetails ? (
+                <Layout product={productDetails} />
+            ) : (
+                <Nopage />
+            ))}
         </Fragment>
     );
 }
